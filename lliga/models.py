8eb5e8590d350040087747f710b334349faf52d7
Juan Antonio Vilaplana <jvilapl4@xtec.cat>
2024-06-26 20:21:19 +0200

    creacio seeder

--- lliga/models.py
+++ lliga/models.py
@@ -27,13 +27,49 @@ class Jugador(models.Model):
         return self.nom
 
 class Partit(models.Model):
-    lliga = models.ForeignKey(Lliga, related_name='partits', on_delete=models.CASCADE)
-    equip_local = models.ForeignKey(Equip, related_name='partits_local', on_delete=models.CASCADE)
-    equip_visitant = models.ForeignKey(Equip, related_name='partits_visitant', on_delete=models.CASCADE)
-    data_partit = models.DateTimeField()
-    gols_local = models.IntegerField(default=0)
-    gols_visitant = models.IntegerField(default=0)
-
+    class Meta:
+        unique_together = ["local","visitant","lliga"]
+    local = models.ForeignKey(Equip,on_delete=models.CASCADE,
+                    related_name="partits_local")
+    visitant = models.ForeignKey(Equip,on_delete=models.CASCADE,
+                    related_name="partits_visitant")
+    lliga = models.ForeignKey(Lliga,on_delete=models.CASCADE)
+    detalls = models.TextField(null=True,blank=True)
+    inici = models.DateTimeField(null=True,blank=True)
     def __str__(self):
-        return f"{self.equip_local} vs {self.equip_visitant} - {self.data_partit.strftime('%Y-%m-%d %H:%M')}"
+        return "{} - {}".format(self.local,self.visitant)
+    def gols_local(self):
+        return self.event_set.filter(
+            tipus=Event.EventType.GOL,equip=self.local).count()
+    def gols_visitant(self):
+        return self.event_set.filter(
+            tipus=Event.EventType.GOL,equip=self.visitant).count()
 
+class Event(models.Model):
+    # el tipus d'event l'implementem amb algo tipus "enum"
+    class EventType(models.TextChoices):
+        GOL = "GOL"
+        AUTOGOL = "AUTOGOL"
+        FALTA = "FALTA"
+        PENALTY = "PENALTY"
+        MANS = "MANS"
+        CESSIO = "CESSIO"
+        FORA_DE_JOC = "FORA_DE_JOC"
+        ASSISTENCIA = "ASSISTENCIA"
+        TARGETA_GROGA = "TARGETA_GROGA"
+        TARGETA_VERMELLA = "TARGETA_VERMELLA"
+    partit = models.ForeignKey(Partit,on_delete=models.CASCADE)
+    temps = models.TimeField()
+    tipus = models.CharField(max_length=30,choices=EventType.choices)
+    jugador = models.ForeignKey(Jugador,null=True,
+                    on_delete=models.SET_NULL,
+                    related_name="events_fets")
+    equip = models.ForeignKey(Equip,null=True,
+                    on_delete=models.SET_NULL)
+    # per les faltes
+    jugador2 = models.ForeignKey(Jugador,null=True,blank=True,
+                    on_delete=models.SET_NULL,
+                    related_name="events_rebuts")
+    detalls = models.TextField(null=True,blank=True)
+
+    
